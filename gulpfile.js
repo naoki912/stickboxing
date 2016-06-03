6bde2d9f94dde14579581609ac643d57ebaa2062
Yuya Otsuka <otsuka.main@google.com>
2016-06-03 09:37:23 +0900

    add:

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+
+var browserSync = require("browser-sync");
+
+gulp.task("default", ['debug']);
+
+gulp.task("debug", () => {
+    browserSync({
+        server: {
+             baseDir: "./build/",
+             index  : "index.html"
+        }
+    });
+});
