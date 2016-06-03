45f5c9b062b43c103ac12a17f1637dcacf2a3561
Yuya Otsuka <otsuka.main@google.com>
2016-06-03 14:37:37 +0900

    fix: gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
-var gulp = require("gulp");
-var browserSync = require("browser-sync");
+var gulp = require("gulp")
+var browserSync = require("browser-sync")
+
+gulp.task("default", ["build"], () => {
+})
+
+gulp.task("build", () => {
+})
+
+gulp.task("test", ["browser-sync"], () => {
+    gulp.watch("build/*.htm", ["browser-sync-reload"])
+    gulp.watch("build/*.css", ["browser-sync-reload"])
+})
 
 gulp.task("browser-sync", () => {
     browserSync({
         server: {
-            baseDir: "./build/",
+            baseDir: "build/",
             index  : "index.htm"
         }
-    });
-});
+    })
+})
 
 gulp.task("browser-sync-reload", () => {
-    browserSync.reload();
-});
- 
-gulp.task("default", ["browser-sync"], () => {
-    gulp.watch("build/*.htm", ['browser-sync-reload']);
-    gulp.watch("build/*.css", ['browser-sync-reload']);
-});
+    browserSync.reload()
+})
