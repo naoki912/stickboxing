aacdf2e9094c02f45d8cf6c543755ed0b02980d1
dhesusan <dhesusan@yahoo.co.jp>
2016-06-07 12:44:19 +0900

    fix: Windowsでタスクが動作しない問題を修正

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gulp = require("gulp")
 gulp.task("default", ["build"])
 
 gulp.task("build", () =>
-    child_process.spawn("npm", ["run", "build"])
+    child_process.spawn("npm", ["run", "build"], { shell: true })
 )
 
 gulp.task("test", ["build"], () =>
-    child_process.spawn("npm", ["run", "test"], { detached: true, stdio: "ignore" }).unref()
+    child_process.spawn("npm", ["run", "test"], { detached: true, shell: true, stdio: "ignore" })
+        .unref()
 )
