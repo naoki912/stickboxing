d117101db75376e6d9a35bcb402e2cca739dfd00
Yuya Otsuka <otsuka.main@google.com>
2016-06-09 09:49:42 +0900

    feat: add member task

--- src/stickboxing/main.js
+++ src/stickboxing/main.js
@@ -6,10 +6,10 @@ window.onload = function () {
     var game = new Game(320, 320)
     game.onload = function () {
         var label = new Label("Stick Boxing")
-        
+
         var scene = new Scene()
         scene.addChild(label)
-        
+
         game.pushScene(scene)
     }
     game.start()
