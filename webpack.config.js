99f7714534ef77d496d2f646a7a54571fc5283ba
Yuya Otsuka <otsuka.main@google.com>
2016-06-09 12:20:27 +0900

    fix: auto generate html

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 var webpack = require("webpack")
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/stickboxing/main.js",
+    entry: {
+        "js/main.js": "./src/stickboxing/main.js",
+        "index.htm": "./res/html/index.htm"
+    },
     output: {
-        filename: "main.js",
-        path: "build/js"
+        filename: "[name]",
+        path: "build"
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             minimize: true
+        }),
+        new htmlWebpackPlugin({
+            title: "Stick Boxing"
         })
     ],
     resolve: {
-        modulesDirectories: [
-            "src",
-            "lib"
-        ]
+        modulesDirectories: ["src", "lib"]
     }
 }
