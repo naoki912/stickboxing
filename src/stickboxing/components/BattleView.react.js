2f34509f936ce684aef7575c3a945d83801377a4
Yuya Otsuka <otsuka.main@google.com>
2016-06-13 12:05:48 +0900

    fix: add dynamic style example

--- src/stickboxing/components/BattleView.react.js
+++ src/stickboxing/components/BattleView.react.js
@@ -5,9 +5,14 @@ var testData = [
 
 ]
 
+var HPBarStyle = (props) => ({
+    width: props.player1.hitPoint / props.player1.maxHitPoint + "px",
+    height: "20px"
+})
+
 export var BattleView = (props) =>
     <div className={styles.Battle}>
-        <div className={HPbar}>
+        <div className={HPbar} style={HPBarStyle(props)}>
             
         </div>
     </div>
