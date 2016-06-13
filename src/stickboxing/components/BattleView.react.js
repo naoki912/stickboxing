66f5ea0ee6ae1240b5910602d2dab8b24f54bd6e
air-foron <kouji19951025@gmail.com>
2016-06-13 11:55:52 +0900

    1110

---
+++ src/stickboxing/components/BattleView.react.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import * as styles from "stickboxing/styles/BattleStyles"
+
+var testData = [
+
+]
+
+export var BattleView = (props) =>
+    <div className={styles.Battle}>
+        <div className={HPbar}>
+            
+        </div>
+    </div>
+
--- src/stickboxing/components/StageView.react.js
+++ src/stickboxing/components/StageView.react.js
@@ -1,14 +1,13 @@
 import * as React from "react"
 import { Link } from "react-router"
+import * as styles from "stickboxing/styles/StageStyles"
 
 export var StageView = (props) =>
     <div className="stage">
-      <img
-        className="image"
-        src={ props.stage.image }
-        style={ { width: "400px", height: "300px" } }>
-      </img>
-      <div className="name">
-      { props.stage.name }
+      <div className={styles.name}>
+       { props.stage.name }
       </div>
+      <img className={styles.image} src={ props.stage.image }>
+      </img>
+      
     </div>
