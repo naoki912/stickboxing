a15aaba24d289e587d30293d20f4eb88d169dfd5
ayunohimono <yuusyuunasakura@yahoo.co.jp>
2016-06-16 11:07:03 +0900

    1116

--- src/stickboxing/components/SettingsView.react.js
+++ src/stickboxing/components/SettingsView.react.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as styles from "stickboxing/styles/SettingsStyles"
+import { settings } from "stickboxing/test/data"
 
 export var SettingsView = (props) =>
     <div className={styles.stageSelect}>
