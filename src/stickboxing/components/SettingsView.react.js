91c768373e53be964cdda452ffc7b2c81ffa5e9b
ayunohimono <yuusyuunasakura@yahoo.co.jp>
2016-06-16 11:24:38 +0900

    1116

--- src/stickboxing/components/SettingsView.react.js
+++ src/stickboxing/components/SettingsView.react.js
@@ -3,10 +3,10 @@ import * as styles from "stickboxing/styles/SettingsStyles"
 import { settings } from "stickboxing/test/data"
 
 export var SettingsView = (props) =>
-    <div className={styles.stageSelect}>
-      <div className={styles.title}>
-        SETTINGS
-      </div>
+    <div className={styles.settings}>
+        <h1 className={styles.title}>
+          Settings
+        </h1>
       <ul>
           <button onClick={props.onSubmit()}></button>
       </ul>
