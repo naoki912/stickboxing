7384062beb92854e0613f4cd0429a50309634368
air-foron <kouji19951025@gmail.com>
2016-06-16 10:40:14 +0900

    1110

--- src/stickboxing/components/StageView.react.js
+++ src/stickboxing/components/StageView.react.js
@@ -3,11 +3,10 @@ import { Link } from "react-router"
 import * as styles from "stickboxing/styles/StageStyles"
 
 export var StageView = (props) =>
-    <div className="stage">
-      <div className={styles.name}>
-       { props.stage.name }
-      </div>
-      <img className={styles.image} src={ props.stage.image }>
-      </img>
-      
+    <div className={styles.stage}>
+     <img className={styles.image} src={ props.stage.image }>
+     </img>
+     <div className={styles.name}>
+      { props.stage.name }
+     </div>  
     </div>
