04eee8e38b20ed0ace2b7da65e2acedd910cd71b
Yuya Otsuka <otsuka.main@google.com>
2016-06-16 10:40:06 +0900

    fix(test): settings

--- src/test/data.js
+++ src/test/data.js
@@ -1,4 +1,4 @@
-export var Settings = {
+export var settings = {
     buttonLayout: {
         allowButtonPosition: {
             x: 100 ,y: 100
@@ -17,3 +17,5 @@ export var Settings = {
         }
     }
 }
+
+
