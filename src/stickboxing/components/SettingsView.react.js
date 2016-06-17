93f0a1579a13a8af799966c0df2285f53434e715
ayunohimono <yuusyuunasakura@yahoo.co.jp>
2016-06-17 09:36:50 +0900

    1116

--- src/stickboxing/components/SettingsView.react.js
+++ src/stickboxing/components/SettingsView.react.js
@@ -4,10 +4,16 @@ import { settings } from "stickboxing/test/data"
 
 export var SettingsView = (props) =>
     <div className={styles.settings}>
+        <img className="titleimage"
+            src="/images/stage1.jpg"
+            style={ { width: "400px", height: "300px" } }>
+        </img>
+      <div className={styles.container}>
         <h1 className={styles.title}>
-          Settings
+            Settings
         </h1>
-      <ul>
-          <button onClick={props.onSubmit}></button>
-      </ul>
+        <ul>
+            <button onClick={props.onSubmit}></button>
+        </ul>
+        </div>
     </div>
