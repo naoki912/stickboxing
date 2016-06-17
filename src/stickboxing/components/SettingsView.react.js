bbe53d2c4932735aff52e95d9a4b1dd8304868ec
ayunohimono <yuusyuunasakura@yahoo.co.jp>
2016-06-17 11:04:25 +0900

    1116

--- src/stickboxing/components/SettingsView.react.js
+++ src/stickboxing/components/SettingsView.react.js
@@ -6,14 +6,26 @@ export var SettingsView = (props) =>
     <div className={styles.settings}>
         <img className="titleimage"
             src="/images/stage1.jpg"
-            style={ { width: "400px", height: "300px" } }>
+            style={ { width: "600px", height: "500px" } }>
         </img>
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-            Settings
-        </h1>
-        <ul>
-            <button onClick={props.onSubmit}></button>
-        </ul>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                Settings
+            </h1>
+            <div className={styles.screen}>
+                <img className="backimage"
+                    src="/images/back.jpg"
+                    style={ { width: "600px", height: "400px" } }>
+                </img>
+                <div>action
+                <img className="idouimage"src="/images/idou.png"   
+                style = {{left: "settings.buttonLayout.allowButtonPosition.x" ,
+                top: "settings.buttonLayout.allowButtonPosition.y"}}>
+                </img>
+                <img className="1image"src="/images/1btn.png"></img>
+                <img className="2image"src="/images/2btn.png"></img>
+                <img className="3image"src="/images/3btn.png"></img>
+                <div>
+            </div>
         </div>
     </div>
