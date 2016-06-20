a906bf63dc025108e34ad2e1d789f6ee9ebb37c5
Yuya Otsuka <otsuka.main@google.com>
2016-06-20 09:18:21 +0900

    fix

--- src/stickboxing/components/CharacterView.react.js
+++ src/stickboxing/components/CharacterView.react.js
@@ -9,7 +9,7 @@ var globeList = [
     { id: 3, name: "Globe4", image: "/images/globe.jpg" }
 ]
 
-var pantList = [
+var pantsList = [
     { id: 0, name: "Pant1", image: "/images/pant.jpg" },
     { id: 1, name: "Pant2", image: "/images/pant.jpg" },
     { id: 2, name: "Pant3", image: "/images/pant.jpg" },
@@ -28,7 +28,7 @@ var hasGlobeList = [
     { id: 1, name: "HasGlobe2", image: "/images/hasGlobe.jpg" }
 ]
 
-var hasPantList = [
+var hasPantsList = [
     { id: 1, name: "HasPaint2", image: "/images/hasPaint.jpg" },
     { id: 3, name: "HasPaint4", image: "/images/hasPaint.jpg" }
 ]
@@ -46,63 +46,31 @@ var testHasGlobeList = [
 
 export var CharacterView = (props) =>
     <div>
-        <div className={styles.title}>
-            Character
-        </div>
-        <ul>
-            GlobeList
-            {globeList.map(
-                (globe) =>
-                    <li>
-                        { globe.name }
-                    </li>
-            )}
-            <br />
-            
-            PantList
-            {pantList.map(
-                (pant) =>
-                    <li>
-                        { pant.name }
-                    </li>
-            )}
-            <br />
-            
-            BeltList
-            {beltList.map(
-                (belt) =>
-                    <li>
-                        { belt.name }
-                    </li>
-            )}
-            <br />
-            
-            HasGlobeList
-            {hasGlobeList.map(
-                (globe) =>
-                    <li>
-                        { globe.name }
-                    </li>
-            )}
-            <br />
-            
-            HasPantList
-            {hasPantList.map(
-                (pant) =>
-                    <li>
-                        { pant.name }
-                    </li>
-            )}
-            <br />
-            
-            HasBeltList
-            {hasBeltList.map(
-                (belt) =>
-                    <li>
-                        { belt.name }
-                    </li>
-            )}
-            <br />
-            
-        </ul>
+      <div className={styles.title}>
+        Character
+      </div>
+      GlobeList
+      <ul>
+        {globeList.map((globe) => <li>{globe.name}</li>)}
+      </ul>
+      PantList
+      <ul>
+        {pantsList.map((pants) => <li>{pants.name}</li>)}
+      </ul>
+      BeltList
+      <ul>
+        {beltList.map((belt) => <li>{belt.name}</li>)}
+      </ul>
+      HasGlobeList
+      <ul>
+        {hasGlobeList.map((globe) => <li>{globe.name}</li>)}
+      </ul>
+      HasPantsList
+      <ul>
+        {hasPantsList.map((pants) => <li>{ pants.name }</li>)}
+      </ul> 
+      HasBeltList
+      <ul>
+        {hasBeltList.map((belt) => <li>{belt.name}</li>)}
+      </ul>
     </div>
