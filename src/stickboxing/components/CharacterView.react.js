117462cd3c34526450488b9e76ba5decd316f66b
ayunohimono <yuusyuunasakura@yahoo.co.jp>
2016-06-30 12:31:11 +0900

    1116

--- src/stickboxing/components/CharacterView.react.js
+++ src/stickboxing/components/CharacterView.react.js
@@ -4,64 +4,43 @@ import { ButtonView } from "stickboxing/components/ButtonView"
 
 
 var globeList = [
-    { id: 0, name: "Globe1", image: "/images/globe.jpg" },
-    { id: 1, name: "Globe2", image: "/images/globe.jpg" },
-    { id: 2, name: "Globe3", image: "/images/globe.jpg" },
-    { id: 3, name: "Globe4", image: "/images/globe.jpg" }
+    { id: 0, name: "Globe1", image: "/images/globe.jpg", has: true },
+    { id: 1, name: "Globe2", image: "/images/globe.jpg", has: false },
+    { id: 2, name: "Globe3", image: "/images/globe.jpg", has: false },
+    { id: 3, name: "Globe4", image: "/images/globe.jpg", has: false }
 ]
 
 var pantsList = [
-    { id: 0, name: "Pant1", image: "/images/pant.jpg" },
-    { id: 1, name: "Pant2", image: "/images/pant.jpg" },
-    { id: 2, name: "Pant3", image: "/images/pant.jpg" },
-    { id: 3, name: "Pant4", image: "/images/pant.jpg" }
+    { id: 0, name: "Pant1", image: "/images/pant.jpg", has: true },
+    { id: 1, name: "Pant2", image: "/images/pant.jpg", has: false },
+    { id: 2, name: "Pant3", image: "/images/pant.jpg", has: false },
+    { id: 3, name: "Pant4", image: "/images/pant.jpg", has: false }
 ]
 
 var beltList = [
-    { id: 0, name: "belt1", image: "/images/belt.jpg" },
-    { id: 1, name: "belt2", image: "/images/belt.jpg" },
-    { id: 2, name: "belt3", image: "/images/belt.jpg" },
-    { id: 3, name: "belt4", image: "/images/belt.jpg" }
+    { id: 0, name: "belt1", image: "/images/belt.jpg", has: true },
+    { id: 1, name: "belt2", image: "/images/belt.jpg", has: false },
+    { id: 2, name: "belt3", image: "/images/belt.jpg", has: false },
+    { id: 3, name: "belt4", image: "/images/belt.jpg", has: false }
 ]
-
-var hasGlobeList = [
-    { id: 0, name: "HasGlobe1", image: "/images/hasGlobe.jpg" },
-    { id: 1, name: "HasGlobe2", image: "/images/hasGlobe.jpg" }
-]
-
-var hasPantsList = [
-    { id: 1, name: "HasPaint2", image: "/images/hasPaint.jpg" },
-    { id: 3, name: "HasPaint4", image: "/images/hasPaint.jpg" }
-]
-
-var hasBeltList = [
-    { id: 0, name: "HasBelt1", image: "/images/hasBelt.jpg" },
-    { id: 2, name: "HasBelt3", image: "/images/hasBelt.jpg" }
-]
-
-var testHasGlobeList = [
-    { id: 0 },
-    { id: 1 }
-]
-
+var money = 100
 
 export var CharacterView = (props) =>
     <div className={styles.character}>
         <div className={styles.title}>
-            Character
+            Character 
         </div>
+var cnt = 30;
+console.log(cnt + "点ゲット！");
         <img src="/images/player.png" width="192" height="192" className={styles.cyara}></img>
-        <ButtonView className={styles.rightAllowButton}
-          onMouseDown={props.onRightAllowButtonPressed}>
-          →
-        </ButtonView>
-        
+        <button className={styles.button} name="btn"> 変更 </button>
         <div >
         <select size = "3" className={styles.list}>
-                {globeList.map((globe) => <option>{globe.name}</option>)}
-                {pantsList.map((pants) => <option>{pants.name}</option>)}
-                {beltList.map((belt) => <option>{belt.name}</option>)}
-                {beltList.map((belt) => <option>{belt.name}</option>)}
+            <option selected="selected">装備</option>
+            {globeList.map((globe) => <option>{globe.name}</option>)}
+            {pantsList.map((pants) => <option>{pants.name}</option>)}
+            {beltList.map((belt) => <option>{belt.name}</option>)}
+            {beltList.map((belt) => <option>{belt.name}</option>)}
         </select>
         </div>
     </div>
