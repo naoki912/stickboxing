7970ae8a910a65f00d73d906d880c1325e919a71
air-foron <kouji19951025@gmail.com>
2016-07-04 12:14:49 +0900

    1110

--- src/stickboxing/components/StageListView.jsx
+++ src/stickboxing/components/StageListView.jsx
@@ -30,7 +30,7 @@ export var StageListView = (props) =>
               </li>
       )}
       </ul>
-      <ButtonView className={styles.selectButton}  onTouchStart={() => props.onStageSelecteListener(selectStage)}>
+      <ButtonView className={styles.selectButton}  onTouchStart={() => props.onStageSelected(stage)}>
       {(selectStageID == null ? null : (selectStageID == 0 ? "Name1に決定" : (selectStageID == 1 ? "Name2に決定" : (selectStageID == 2 ? "Name3に決定" : (selectStageID == 3 ? "Name4に決定" : "Name5に決定")))))}
       </ButtonView>
     </div>
