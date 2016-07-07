b11166d9ef16402f3b8d06b34929d7cfe5413823
Yuya Otsuka <otsuka.main@google.com>
2016-07-07 12:45:04 +0900

    fix: import

--- src/stickboxing/components/CharacterView.jsx
+++ src/stickboxing/components/CharacterView.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import styles from "stickboxing/styles/CharacterStyles.css"
-import { ButtonView } from "stickboxing/components/ButtonView"
+import ButtonView from "stickboxing/components/ButtonView"
 
 
 var globeList = [
