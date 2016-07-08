a389798f26819dd94a9d79c0ea79ea346366d8e2
air-foron <kouji19951025@gmail.com>
2016-07-08 10:47:17 +0900

    1110

--- src/stickboxing/components/StagesViewContainer.jsx
+++ src/stickboxing/components/StagesViewContainer.jsx
@@ -10,7 +10,7 @@ export default class extends React.Component {
             stages: [
                 Stage({id: 0, name: "Name1", image: "/images/stage0.svg"}),
                 Stage({id: 1, name: "Name2", image: "/images/stage0.jpg"}),
-                Stage({id: 2, name: "Name3", image: "/images/stage1.jpg"}),
+                Stage({id: 2, name: "Name3", image: "/images/stage1.svg"}),
                 Stage({id: 3, name: "Name4", image: "/images/stage1.jpg"}),
                 Stage({id: 4, name: "Name5", image: "/images/stage1.jpg"})
             ],
